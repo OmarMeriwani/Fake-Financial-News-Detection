7a6265b433211ef20ead8ee1b4b6cb04a78e732b
OmarMeriwani <omar.sirwan@gmail.com>
2019-04-03 23:06:40 +0100

    cleaning the text

--- Sentiments/lexicalFeatures.py
+++ Sentiments/lexicalFeatures.py
@@ -1,3 +1,41 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk.tokenize
+import re, string, timeit
+
+df = pd.read_csv('EnglishGS.csv',header=0)
+'''Unique,Text,Average Score,Type,source_id'''
+#print(df['Text'])
+
+dfResult = pd.DataFrame(columns=['Unique','Text','Average Score','Type','source_id','Tokens','LN3','LN4','LN5'])
+for i in range(0,len(df)):
+
+    text = df.loc[i].values[1]
+    print('BEFORE',text)
+
+    text = str(text).replace('\n','')
+    text = str(text).replace('\\n','')
+    '''Remove NER'''
+
+    newSentence = re.sub("\$[A-Z]+", " ", str(text))
+    newSentence = re.sub(" [A-Z]{2}[A-Z]+ ", " ", newSentence)
+
+    '''Lower case'''
+    newSentence = ' '.join( [t.strip().lower() for t in str(text).split(' ')])
+
+    '''Remove double spaces'''
+    newSentence = newSentence.replace('  ','')
+    '''Remove time'''
+    newSentence = re.sub(r'\d+\:\d+ (p|a)m', '', newSentence)
+    '''Remove links'''
+    newSentence = re.sub(r'http\S+', '', newSentence)
+    '''Remove special characters'''
+    newSentence = newSentence.translate(str.maketrans('', '', string.punctuation))
+    '''Remove numbers'''
+    newSentence = re.sub("\d+\$|\d+", " ", newSentence)
+    '''Remove multiple spaces'''
+    newSentence = re.sub(' +', ' ',newSentence)
+    print('AFTER',newSentence)
+
+    '''Tokenization'''
